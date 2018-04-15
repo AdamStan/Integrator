126024d7fda4da4ec6c4b1c07e6ef9a846dc5227
AdamStan <adam15stan@gmail.com>
2018-04-15 12:39:59 +0200

    Iteration into datatable was added

--- Integrator/Integrator.cs
+++ Integrator/Integrator.cs
@@ -39,22 +39,44 @@ namespace Integrator
                 namesOfTables.Add(tablename);
             }
 
-            data = new DataSet();
+            DataSet data1 = new DataSet();
+            DataSet data2 = new DataSet();
 
-            foreach (String name in namesOfTables)
+            for(int i = 0; i < namesOfTables.Count; i++)
             {
-                String command = "Select * from " + name;
+                String command = "Select * from " + namesOfTables[i];
                 SqlDataAdapter adp1 = new SqlDataAdapter(command, con1);
                 SqlDataAdapter adp2 = new SqlDataAdapter(command, con2);
-                adp1.Fill(data);
-                adp2.Fill(data);
+                DataTable dt1 = new DataTable();
+                DataTable dt2 = new DataTable();
+                adp1.Fill(dt1);
+                adp2.Fill(dt2);
+                data1.Tables.Add(dt1);
+                data2.Tables.Add(dt2);
             }
 
-            //DataView = new DataGridView();
-            //DataView.DataSource = set;
-            //DataView.DataMember = "Customers";
+            
+            foreach(DataTable table in data1.Tables)
+            {
+                DataRowCollection wiersze = table.Rows;
+                DataColumnCollection columny = table.Columns;
+                foreach (DataRow row in wiersze)
+                {
+                    foreach (DataColumn col in columny)
+                    {
+                        Console.Write(row[col] + ", ");
+                    }
+                    Console.WriteLine(";");
+                }
+            }
 
             Console.WriteLine();
+            data = new DataSet();
+
+            data.Merge(data1);
+            data.Merge(data2); //nie dodaje bo te same kolumny primary key
+
+
             //SqlCommand 
             //SqlDataAdapter  
             //dateset'y i jakoś to integrować
